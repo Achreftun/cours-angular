fccb3c7223d3d5043e55bc37488cda7a44fbf083
el mouelhi <elmouelhi.achref@gmail.com>
2022-06-28 14:35:36 +0200

    ajout de moyennes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   title = 'cours-angular';
   stagiaire: Stagiaire = new Stagiaire(100, "Wick");
   tab: number[] = [2, 3, 8, 5];
+  moyennes = [18, 5, 11, 15];
   stagiaires: Array<Stagiaire> = [
     new Stagiaire(100, 'Wick', 'John'),
     new Stagiaire(101, 'Abruzzi', 'John'),
