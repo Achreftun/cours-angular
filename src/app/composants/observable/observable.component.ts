896443edd1eaee7f9b1f396ee14824e38757d2e5
el mouelhi <elmouelhi.achref@gmail.com>
2022-06-28 16:40:20 +0200

    premier exemple avec observable

--- src/app/composants/observable/observable.component.ts
+++ src/app/composants/observable/observable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -11,6 +12,13 @@ export class ObservableComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const observable: Observable<number> = interval(2000);
+    const observer: Observer<number> = {
+      next: (val) => this.tab.push(val),
+      error: (err) => this.status = err,
+      complete: () => this.status = 'fini'
+    }
+    observable.subscribe(observer);
   }
 
 }
