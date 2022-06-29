6fad5a739bfd73ead8c5ecdc162fd226e1147a74
el mouelhi <elmouelhi.achref@gmail.com>
2022-06-29 16:07:08 +0200

    even-value correction 2

--- src/app/modules/shared/even-value.pipe.ts
+++ src/app/modules/shared/even-value.pipe.ts
@@ -6,7 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class EvenValuePipe implements PipeTransform {
 
   transform(value: number[], ...args: unknown[]): number[] {
-    return value.filter(elt => elt % 2 == 0);
+    let resultat: number[] = [];
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] % 2 == 0) {
+        resultat.push(value[i])
+      }
+    }
+    return resultat;
   }
 
 }
