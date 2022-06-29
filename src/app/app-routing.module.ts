42d4bf9cf8dc604d5489db8bf46500b6881b7460
el mouelhi <elmouelhi.achref@gmail.com>
2022-06-29 09:14:56 +0200

    routage première partie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdresseComponent } from './composants/adresse/adresse.component';
+import { ObservableComponent } from './composants/observable/observable.component';
+import { StagiaireComponent } from './composants/stagiaire/stagiaire.component';
 
-const routes: Routes = [];
+
+// mapping path component
+const routes: Routes = [
+  { path: 'adresse', component: AdresseComponent },
+  { path: 'stagiaire', component: StagiaireComponent },
+  { path: 'observable', component: ObservableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ObservableComponent } from './composants/observable/observable.component';
+import { AdresseComponent } from './composants/adresse/adresse.component';
+import { StagiaireComponent } from './composants/stagiaire/stagiaire.component';
+import { MenuComponent } from './composants/menu/menu.component';
 
 // module principal d'Angular
 // Par défaut tout est défini dans le module principal
@@ -13,7 +16,10 @@ import { ObservableComponent } from './composants/observable/observable.componen
 @NgModule({
   declarations: [
     AppComponent,
-    ObservableComponent
+    ObservableComponent,
+    AdresseComponent,
+    StagiaireComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, // CommonModule + tout ce qui concerne le navigateur
