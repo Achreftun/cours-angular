0066bb59912d67804c680bb0dcbf8c1826e17a0b
el mouelhi <elmouelhi.achref@gmail.com>
2022-06-29 16:13:30 +0200

    création de str-to-array

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GetCharPipe } from './get-char.pipe';
 import { EvenValuePipe } from './even-value.pipe';
+import { StrToArrayPipe } from './str-to-array.pipe';
 
 
 
 @NgModule({
   declarations: [
     GetCharPipe,
-    EvenValuePipe
+    EvenValuePipe,
+    StrToArrayPipe
   ],
   imports: [
     CommonModule
