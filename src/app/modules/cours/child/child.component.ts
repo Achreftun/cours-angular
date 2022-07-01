35827d8d52d7fc8f7bd9b1c0f7775758f7416a0d
el mouelhi <elmouelhi.achref@gmail.com>
2022-07-01 14:13:21 +0200

    @Output exemple complet

--- src/app/modules/cours/child/child.component.ts
+++ src/app/modules/cours/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
-
+  @Input() nom = '';
+  note = 0;
+  buttonStatus = false;
+  @Output() message = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  envoyer() {
+    this.message.emit(this.note);
+    this.buttonStatus = true;
+  }
 }
--- src/app/modules/cours/parent/parent.component.ts
+++ src/app/modules/cours/parent/parent.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
-
+  noms = ['wick', 'travolta', 'abruzzi'];
+  moyenne = 0;
+  somme = 0;
+  nbrNotes = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  calculerMoyenne(valeur: number) {
+    this.somme += valeur;
+    this.nbrNotes++;
+    this.moyenne = this.somme / this.nbrNotes;
+  }
 }
