c4eb6545431010124f4c6323c319e49f8adaee3e
el mouelhi <elmouelhi.achref@gmail.com>
2022-07-01 09:33:09 +0200

    ajout de HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { PersonneService } from './services/personne.service';
   imports: [
     BrowserModule, // CommonModule + tout ce qui concerne le navigateur
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [], // services
   bootstrap: [AppComponent]
