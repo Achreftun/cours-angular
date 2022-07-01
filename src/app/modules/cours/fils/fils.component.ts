0ad389f0bf0c3b2f8bd45a00fac9569030b855d2
el mouelhi <elmouelhi.achref@gmail.com>
2022-07-01 11:54:52 +0200

    ngOnChanges

--- src/app/modules/cours/fils/fils.component.ts
+++ src/app/modules/cours/fils/fils.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-fils',
   templateUrl: './fils.component.html',
   styleUrls: ['./fils.component.css']
 })
-export class FilsComponent implements OnInit {
+export class FilsComponent implements OnInit, OnChanges {
   @Input() ordre = '';
   @Input() ville = '';
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    for (const key in changes) {
+      console.log(key)
+      const change = changes[key];
+      const value = change.currentValue;
+      const previous = change.previousValue;
+      console.log(value, previous)
+    }
+  }
 
   ngOnInit(): void {
   }
